666aee50cacf479ba77770aa842525d260ebc061
Goravit Buakampun <kamper541@Goravits-MacBook-Pro.local>
2019-12-17 16:36:57 +0700

    Test123

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('asdfasdfasdfsdfsdfas')
+
+        let srt ='';
+        for(let i = 0 ; i < 10 ; i ++){
+            console.log(i+1);
+            srt += (i+1) + ''
+        }
+
+        console.log(srt);
+
+        document.querySelector('#output').innerHTML = srt;
